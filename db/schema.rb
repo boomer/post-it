25aefd68efbc6965ed9e5f16432c3654fb95d2d6
Michael Boomershine <boomer@shorttakes.com>
2015-03-09 22:49:57 -0700

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310051612) do
+ActiveRecord::Schema.define(version: 20150310054227) do
 
   create_table "posts", force: true do |t|
     t.string "title"
     t.string "url"
     t.text   "description"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
   end
 
 end
